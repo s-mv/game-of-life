d90690db3c970e818aeb8ab9fc413a19ee7fffc6
Shreerang Vaidya <smv@pop-os.localdomain>
2022-09-11 21:54:23 +0530

    fixed 'disallowed MIME type' issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './src',
+  base: '/game-of-life/',
   build: {
     outDir: '../',
     rollupOptions: {
